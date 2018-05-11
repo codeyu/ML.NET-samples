c4600409c8662714cb9ba147e44551b10bb452dd
codeyu <codeyu@gmail.com>
2018-05-11 23:05:02 +0800

    2&3 init

---
+++ src/2.0SentimentAnalysis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2._0SentimentAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/3.0TaxiFarePredictor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3._0TaxiFarePredictor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
